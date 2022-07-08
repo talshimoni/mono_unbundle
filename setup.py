5be6509940b1d64f19d55810a21cd9211b70bad6
Restyled.io <commits@restyled.io>
2022-07-08 06:35:45 +0000

    Restyled by black

--- setup.py
+++ setup.py
@@ -6,44 +6,36 @@ from io import open
 
 here = path.abspath(path.dirname(__file__))
 pfile = Project(__file__).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
+requirements = convert_deps_to_pip(pfile["packages"], r=False)
 print(requirements)
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='mono_unbundle',
-    version='2019.03.10.dev0',
-    description='Extract DLLs from Xamarin app bundles',
+    name="mono_unbundle",
+    version="2019.03.10.dev0",
+    description="Extract DLLs from Xamarin app bundles",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/tjg1/mono_unbundle',
-    author='Tomasz J Goralczyk',
-    author_email='tomg@fastmail.uk',
+    long_description_content_type="text/markdown",
+    url="https://github.com/tjg1/mono_unbundle",
+    author="Tomasz J Goralczyk",
+    author_email="tomg@fastmail.uk",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Developers',
-        'Topic :: Security',
-        'Topic :: Software Development :: Disassemblers',
-        'Topic :: System :: Archiving :: Packaging',
-        'Operating System :: Android',
-        'Programming Language :: C#'
-
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
+        "Topic :: Software Development :: Disassemblers",
+        "Topic :: System :: Archiving :: Packaging",
+        "Operating System :: Android",
+        "Programming Language :: C#" "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='reverse_engineering android xamarin apk',
+    keywords="reverse_engineering android xamarin apk",
     packages=find_packages(),
-    python_requires='>=3.5',
+    python_requires=">=3.5",
     install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'mono_unbundle=mono_unbundle.cli:cli'
-        ]
-    }
+    entry_points={"console_scripts": ["mono_unbundle=mono_unbundle.cli:cli"]},
 )
